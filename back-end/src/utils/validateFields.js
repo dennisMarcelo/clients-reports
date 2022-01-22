00face3db27f1be4a3d41bd6172a04eabd44f543
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-22 14:57:50 -0300

    rules to create new user updated

--- back-end/src/utils/validateFields.js
+++ back-end/src/utils/validateFields.js
@@ -12,7 +12,7 @@ const newUser = (user) => {
     userName: Joi.string().min(3).max(150).required(),
     birth: Joi.string().pattern(/\d{4}-\d{2}-\d{2}/).required(),
     registrationDate: Joi.string().pattern(/\d{4}-\d{2}-\d{2}/).required(),
-    familyIncome: Joi.number().min(0).positive(),
+    familyIncome: Joi.number().min(0).required(),
   }).validate(user);
 
   if (error) throw new CustomError(error.message, 400);
