23bc1c3b595c9aad3095a712ba027822c3efa045
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-22 16:47:44 -0300

    fixing erros

--- back-end/src/utils/validateFields.js
+++ back-end/src/utils/validateFields.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 const CustomError = require('./CustomError');
 
-const user = (user) => {
-  console.log(user);
+const user = (data) => {
+  console.log(data);
   const { error } = Joi.object({
     cpf: Joi.string()
       .min(11)
@@ -13,7 +13,7 @@ const user = (user) => {
     birth: Joi.string().pattern(/\d{4}-\d{2}-\d{2}/).required(),
     registrationDate: Joi.string().pattern(/\d{4}-\d{2}-\d{2}/).required(),
     familyIncome: Joi.number().min(0).required(),
-  }).validate(user);
+  }).validate(data);
 
   if (error) throw new CustomError(error.message, 400);
 };
